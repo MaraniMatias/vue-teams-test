875fa573cdbbfa83e6204a348b8228d00b1ba9b8
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-18 15:21:22 -0300

    Extract TeamGrid component

---
+++ components/TeamGrid.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-layout column justify-center>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" lg="10" class="pa-0 mb-0">
+        <v-layout justify-center align-center wrap>
+          <v-flex xs12 lg3 class="px-2">
+            <v-text-field
+              v-model.lazy="query"
+              label="Buscar"
+              hide-details
+              clearable
+              :readonly="loading"
+            />
+          </v-flex>
+          <!--
+          <v-flex xs12 lg3 class="px-2">
+            <v-text-field label="Filtrar" hide-details :readonly="loading" />
+          </v-flex>
+          -->
+          <v-flex xs12 lg3 class="px-2">
+            <v-select
+              v-model.lazy="orderKey"
+              label="Ordenar"
+              hide-details
+              :items="orderKeyList"
+              item-value="key"
+              item-text="text"
+              clearable
+              :readonly="loading"
+            />
+          </v-flex>
+          <v-flex xs12 lg3>
+            <v-layout align-center class="mt-3" fill-height justify-end>
+              <p class="mb-0 mx-2" v-text="showPageInfo" />
+              <v-btn icon :disabled="disableBeforeBtn" @click="beforePage">
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-btn icon :disabled="disableNextBtn" @click="nextPage">
+                <v-icon>mdi-chevron-right</v-icon>
+              </v-btn>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-flex v-show="loading" xs12 class="text-center my-12">
+        <v-progress-circular
+          width="2"
+          indeterminate
+          active
+          color="grey darken-1"
+        />
+        <p class="mt-6">Buscando...</p>
+      </v-flex>
+    </v-row>
+    <v-row justify="center">
+      <v-col v-for="team in items" :key="team.id" cols="auto" class="pb-0 mb-0">
+        <CardTeam :team="team" />
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
+
+<script>
+/// <reference path="./../types/index.js" />
+import CardTeam from './../components/CardTeam'
+
+export default {
+  components: { CardTeam },
+  props: {
+    /** @type {{ Array<Team> }} */
+    items: { type: Array, required: true },
+    page: { type: Number, default: 0 },
+    itemsPrePage: { type: Number, default: 9 },
+    totalItems: { type: Number, default: 0 },
+    loading: { type: Boolean, default: false },
+  },
+  data: () => ({
+    query: null,
+    orderKey: '',
+    orderKeyList: [
+      { key: 'id', text: 'Código' },
+      { key: 'mascot', text: 'Mascota' },
+      { key: 'school', text: 'Escuela' },
+      { key: 'color', text: 'Color' },
+    ],
+  }),
+  computed: {
+    showPageInfo() {
+      return `Pag.: ${this.page + 1} - Equipo: ${this.totalItems}`
+    },
+    disableBeforeBtn() {
+      return this.loading || this.page === 0
+    },
+    disableNextBtn() {
+      return (
+        this.loading || this.page * this.itemsPrePage * 2 >= this.totalItems
+      )
+    },
+  },
+  watch: {
+    query() {
+      this.$emit('search', this.query)
+    },
+    orderKey() {
+      this.$emit('order', this.orderKey)
+    },
+  },
+  created() {},
+  methods: {
+    beforePage() {
+      this.$emit('before')
+    },
+    nextPage() {
+      this.$emit('next')
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,23 @@
 <template>
-  <v-layout column justify-center>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" lg="10" class="pa-0 mb-0">
-        <v-layout justify-center align-center wrap>
-          <v-flex xs12 lg3 class="px-2">
-            <v-text-field
-              v-model.lazy="query"
-              label="Buscar"
-              hide-details
-              clearable
-              :readonly="loading"
-            />
-          </v-flex>
-          <!--
-          <v-flex xs12 lg3 class="px-2">
-            <v-text-field label="Filtrar" hide-details :readonly="loading" />
-          </v-flex>
-          -->
-          <v-flex xs12 lg3 class="px-2">
-            <v-select
-              v-model.lazy="orderKey"
-              label="Ordenar"
-              hide-details
-              :items="orderKeyList"
-              item-value="key"
-              item-text="text"
-              clearable
-              :readonly="loading"
-            />
-          </v-flex>
-          <v-flex xs12 lg3>
-            <v-layout align-center class="mt-3" fill-height justify-end>
-              <p class="mb-0 mx-2" v-text="showPageInfo" />
-              <v-btn icon :disabled="disableBeforeBtn" @click="beforePage">
-                <v-icon>mdi-chevron-left</v-icon>
-              </v-btn>
-              <v-btn icon :disabled="disableNextBtn" @click="nextPage">
-                <v-icon>mdi-chevron-right</v-icon>
-              </v-btn>
-            </v-layout>
-          </v-flex>
-        </v-layout>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <v-flex v-show="loading" xs12 class="text-center my-12">
-        <v-progress-circular
-          width="2"
-          indeterminate
-          active
-          color="grey darken-1"
-        />
-        <p class="mt-6">Buscando...</p>
-      </v-flex>
-    </v-row>
-    <v-row justify="center">
-      <v-col
-        v-for="team in filterItems"
-        :key="team.id"
-        cols="auto"
-        class="pb-0 mb-0"
-      >
-        <CardTeam :team="team" />
-      </v-col>
-    </v-row>
-  </v-layout>
+  <TeamGrid
+    :items="filterItems"
+    :page="page"
+    :items-pre-page="itemsPrePage"
+    :total-items="totalElements"
+    :loading="loading"
+    @before="beforePage"
+    @next="nextPage"
+    @search="search"
+    @order="order"
+  />
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
-import CardTeam from './../components/CardTeam'
+import TeamGrid from './../components/TeamGrid'
 
 export default {
-  components: { CardTeam },
+  components: { TeamGrid },
   asyncData() {},
   data: () => ({
     filterItems: [],
@@ -80,32 +25,9 @@ export default {
     page: 0,
     itemsPrePage: 9,
     loading: true,
-    query: null,
-    orderKeyList: [
-      { key: 'id', text: 'Código' },
-      { key: 'mascot', text: 'Mascota' },
-      { key: 'school', text: 'Escuela' },
-      { key: 'color', text: 'Color' },
-    ],
-    orderKey: '',
   }),
   computed: {
     ...mapState({ teams: 'filterItems' }),
-    showPageInfo() {
-      return `Pag.: ${this.page + 1} - Equipo: ${this.totalElements}`
-    },
-    disableBeforeBtn() {
-      return this.loading || this.page === 0
-    },
-    disableNextBtn() {
-      return (
-        this.loading || this.page * this.itemsPrePage * 2 >= this.totalElements
-      )
-    },
-  },
-  watch: {
-    query: 'search',
-    orderKey: 'order',
   },
   async mounted() {
     const { error } = await this.getTeams()
@@ -140,13 +62,13 @@ export default {
         this.page += 1
       }
     },
-    async search() {
-      await this.filterTeams(this.query)
+    async search(query) {
+      await this.filterTeams(query)
       this.reloadItems()
     },
-    async order() {
+    async order(orderKey) {
       this.loading = true
-      await this.orderByTeams(this.orderKey)
+      await this.orderByTeams(orderKey)
       this.reloadItems()
       this.loading = false
     },
