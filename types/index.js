61f203733bfcc3bead511007c8e1eed219bcd46e
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-18 09:01:57 -0300

    Add Alert doc on types file

---
+++ types/index.js
@@ -0,0 +1,12 @@
+/// <reference path="../utils/enum/TipoUsuario.js" />
+
+/**
+ *  Alert
+ * this.$notify({ ... })
+ * @typedef {Object} Alert
+ * @property {String} text
+ * @property {String} color
+ * @property {Number} timeout Timeout for dimension alert, Default: 7000 is error
+ * @property {String} type  Values [info|success|error|warn|none]
+ * @property {Boolean} close  Humans can close alert
+ */
