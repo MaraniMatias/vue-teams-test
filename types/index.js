48789ecb7fb13d4154e3af77c8132879f8a1c18d
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-18 13:49:36 -0300

    add team object docs

--- types/index.js
+++ types/index.js
@@ -10,3 +10,20 @@
  * @property {String} type  Values [info|success|error|warn|none]
  * @property {Boolean} close  Humans can close alert
  */
+
+/**
+ * Equipo
+ * @typedef {Object} Team
+ * @property {Number} id
+ * @property {String} school
+ * @property {String} mascot
+ * @property {String} abbreviation
+ * @property {String} conference
+ * @property {String} division
+ * @property {String} color
+ * @property {String} alt_color
+ * @property {String[]} logos
+ * @property {String} alt_name_1
+ * @property {String} alt_name_2
+ * @property {String} alt_name_3
+ */
