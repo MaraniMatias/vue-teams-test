b72bdb4585663c914ea57c062623032b23b6cf57
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-18 17:12:42 -0300

    Persistent query on store

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,30 @@
 <template>
   <TeamGrid
-    :items="filterItems"
+    :items="itemsOnPage"
     :page="page"
     :items-pre-page="itemsPrePage"
     :total-items="totalElements"
     :loading="loading"
-    @before="beforePage"
-    @next="nextPage"
+    @before="beforeItems"
+    @next="nextItems"
     @search="search"
     @order="order"
   />
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import { mapGetters, mapActions, mapState } from 'vuex'
 import TeamGrid from './../components/TeamGrid'
 
 export default {
   components: { TeamGrid },
   asyncData() {},
   data: () => ({
-    filterItems: [],
-    totalElements: 0,
-    page: 0,
-    itemsPrePage: 9,
     loading: true,
   }),
   computed: {
-    ...mapState({ teams: 'filterItems' }),
+    ...mapState(['itemsPrePage', 'page']),
+    ...mapGetters(['totalElements', 'itemsOnPage']),
   },
   async mounted() {
     const { error } = await this.getTeams()
@@ -35,41 +32,15 @@ export default {
       this.$notify({ type: 'error', text: 'No pudimos listar los equipos' })
     }
     this.loading = false
-    this.reloadItems()
   },
   methods: {
-    ...mapActions(['getTeams', 'filterTeams', 'orderByTeams']),
-    reloadItems() {
-      this.page = 0
-      this.totalElements = this.teams.length
-      this.filterItems = this.teams.slice(0, this.itemsPrePage)
-    },
-    beforePage() {
-      const start = this.page * this.itemsPrePage - this.itemsPrePage
-      const end = start + this.itemsPrePage
-      this.filterItems = this.teams.slice(start, end)
-      if (start > 0) {
-        this.page -= 1
-      } else {
-        this.page = 0
-      }
-    },
-    nextPage() {
-      const start = this.page * this.itemsPrePage + this.itemsPrePage
-      const end = start + this.itemsPrePage
-      this.filterItems = this.teams.slice(start, end)
-      if (start < this.totalElements) {
-        this.page += 1
-      }
-    },
+    ...mapActions(['getTeams', 'orderByTeams', 'nextItems', 'beforeItems']),
     async search(query) {
       await this.filterTeams(query)
-      this.reloadItems()
     },
     async order(orderKey) {
       this.loading = true
       await this.orderByTeams(orderKey)
-      this.reloadItems()
       this.loading = false
     },
   },
--- store/index.js
+++ store/index.js
@@ -3,38 +3,51 @@ import FilterItems from '~/utils/FilterTeams'
 import OrderByTeams from '~/utils/OrderByTeams'
 
 export const state = () => ({
-  teams: [],
+  items: [],
   filterItems: [],
-  // totalElements: 0,
-  // page: 0,
-  // itemsPrePage: 9,
+  itemsPrePage: 9,
+  page: 0,
 })
 
 export const getters = {
   findById: (state) => (teamId) => {
-    return state.teams.find((team) => team.id === teamId)
+    return state.items.find((team) => team.id === teamId)
+  },
+  totalElements: (state) => state.filterItems.length,
+  itemsOnPage: (state) => {
+    const start = state.page * state.itemsPrePage * 2
+    const end = start + state.itemsPrePage
+    return state.filterItems.slice(start, end)
   },
 }
 
 export const mutations = {
   SET_TEAMS(state, teams) {
-    // state.teams = teams.splice(0, 20)
-    state.teams = teams
+    state.items = teams || []
     state.filterItems = teams.slice(0)
   },
   SET_FILTER(state, teams) {
-    state.filterItems = teams
+    state.filterItems = teams || []
   },
   CLEANER_FILTER(state) {
-    state.filterItems = state.teams.slice(0)
+    state.filterItems = state.items.slice(0)
+  },
+  SET_PAGE(state, page) {
+    state.page = page
+  },
+  //
+  ORDER_BY(state, orderKey) {
+    state.filterItems = OrderByTeams(orderKey, state.filterItems)
   },
 }
 
 export const actions = {
-  async getTeams({ commit }) {
+  async getTeams({ commit, getters }) {
     try {
+      if (getters.totalElements) return { data: state.items }
       const { data } = await Teams.get()
       commit('SET_TEAMS', data || [])
+      commit('SET_PAGE', 0)
       return { data }
     } catch (resp) {
       return { error: resp.error }
@@ -42,18 +55,33 @@ export const actions = {
   },
   filterTeams({ commit, state }, query) {
     if (query) {
-      const filterItems = FilterItems(query, state.filterItems)
+      const filterItems = FilterItems(query, state.items)
       commit('SET_FILTER', filterItems || [])
     } else {
       commit('CLEANER_FILTER')
     }
+    commit('SET_PAGE', 0)
   },
-  orderByTeams({ commit, state }, orderKey) {
+  orderByTeams({ commit }, orderKey) {
     if (orderKey) {
-      const filterItems = OrderByTeams(orderKey, state.filterItems)
-      commit('SET_FILTER', filterItems || [])
+      commit('ORDER_BY', orderKey)
     } else {
       commit('CLEANER_FILTER')
+    }
+    commit('SET_PAGE', 0)
+  },
+  beforeItems({ commit, state }) {
+    const start = state.page * state.itemsPrePage - state.itemsPrePage
+    if (start > 0) {
+      commit('SET_PAGE', state.page - 1)
+    } else {
+      commit('SET_PAGE', 0)
+    }
+  },
+  nextItems({ commit, state, getters }) {
+    const start = state.page * state.itemsPrePage + state.itemsPrePage
+    if (start < getters.totalElements) {
+      commit('SET_PAGE', state.page + 1)
     }
   },
 }
